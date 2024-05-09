0d303f8c191913067e9321414e1b6a9f4a5da9b7
Lilu Cao <lilu.cao@orbitfin.ai>
2024-05-09 16:29:41 +0800

    add oldio

---
+++ src/main/java/com/lilu/io/oldio/IODemo.java
@@ -0,0 +1,28 @@
+package com.lilu.io.oldio;
+
+import java.io.*;
+
+public class IODemo {
+    public static void main(String[] args) throws IOException {
+        testBasicFileIO();
+        testBufferedFileIO();
+    }
+
+    public static void testBasicFileIO() throws IOException {
+        File file = new File("abc.txt");
+        FileOutputStream out = new FileOutputStream(file);
+        while (true) {
+            // 每次写入都会进行 system call 访问 pagecache
+            out.write("abcdefg\n".getBytes());
+        }
+    }
+
+    public static void testBufferedFileIO() throws IOException {
+        File file = new File("abc.txt");
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        while (true) {
+            // 每次写入先会 cache 到 jvm 开辟的缓存中，缓存满了之后再通过 system call 访问 pagecache
+            out.write("abcdefg\n".getBytes());
+        }
+    }
+}
