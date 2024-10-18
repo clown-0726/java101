669739ae61cd966571a1b1c97370338751327dec
Lilu Cao <lilu.cao@orbitfin.ai>
2024-10-18 21:32:56 +0800

    rename netty as ionetty

--- src/main/java/com/lilu/ionetty/helloworld/ClientHandlerHello.java
+++ src/main/java/com/lilu/ionetty/helloworld/ClientHandlerHello.java
@@ -1,13 +1,14 @@
 package com.lilu.netty.helloworld;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * byteBuf 使用了零拷贝技术，也是 netty 高性能的原因之一
  */
-
+@ChannelHandler.Sharable
 public class ClientHandlerHello extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
